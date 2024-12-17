af8836806ad79dcd671cb90171082f22180e53fe
kofiad <kofiadaba@gmail.com>
2024-12-17 15:59:41 +0000

    Refactor UsersController import and routes to UserController

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 // routes/index.js
 import express from 'express';
 import AppController from '../controllers/AppController.js';
-import UsersController from '../controllers/UsersController.js';
+import UserController from '../controllers/UserController.js';
 import AuthController from '../controllers/AuthController.js';
 import ProductsController from '../controllers/ProductsController.js';
 import ProductVariantController from '../controllers/ProductVariantController.js';
@@ -15,8 +15,8 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 //user controller routes
-router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
+router.post('/users', UserController.postNew);
+router.get('/users/me', UserController.getMe);
 //auth controller routes
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
